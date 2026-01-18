69efabad1c9724f97ddcf1a6484189e3abd95c94
YvanCedric <ivancedric82@gmail.com>
2026-01-18 15:00:23 +0000

    build

--- build.sh
+++ build.sh
@@ -1,18 +1,4 @@
-#!/usr/bin/env bash
-# Script de build pour Render
-# Ce script est exécuté lors du build de l'application sur Render
-
-# Installer les dépendances
-pip install -r projetdepartage/requirements.txt
-
-# Collecter les fichiers statiques
+pip install -r requirements.txt
 cd projetdepartage
 python manage.py collectstatic --noinput
-
-# Exécuter les migrations
 python manage.py migrate --noinput
-
-# Revenir au répertoire racine
-cd ..
-
-echo "Build terminé avec succès !"
